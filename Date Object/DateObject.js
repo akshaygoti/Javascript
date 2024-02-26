a9ecb6059950d5df4f5fddb7a139376d0acf8b02
Akshay <akshaygoti23@gmail.com>
2024-02-26 08:14:41 +0530

    26-2-24

---
+++ Date Object/DateObject.js
@@ -0,0 +1,13 @@
+// let date = new Date();
+let date = new Date("2023-11-15 5:24:10");
+// let date = new Date(2025,11,23);
+// let date = new Date(2025,11,23,10);
+// let date = new Date(2025,11,23,10,30,25);
+// date.toString()
+// console.log(date);
+
+// let get = new Date();
+// date.getFullYear();
+// console.log(date.getFullYear());
+
+// let date = new Date(2015)
---
+++ Date Object/get.date.js
@@ -0,0 +1,62 @@
+// fullyear
+
+// let get = new Date();
+// date.getFullYear();
+// console.log(date.getFullYear());
+
+// const date = new Date('August 19, 1975 23:15:30');
+// const date1 = date.getDate();
+
+// console.log(date1);
+
+// let get = new Date();
+// get=getMonth();
+// console.log(get.getMonth());
+
+// let date = new Date()
+// date=getDate()
+// console.log();
+
+// let date = new Date();
+// date.setDate();
+// console.log(date.setDate());
+
+let a = new Date;
+
+// console.log(a.getTime());
+
+
+// console.log(a.getDate(21));                              // => 23
+// console.log(a.getUTCDate(21));                           // => 23
+
+
+// console.log(a.getMonth());                              // => 1
+// console.log(a.getUTCMonth(5));                         // => 1
+
+
+// console.log(a.getFullYear());                          // => 2024
+// console.log(a.getUTCFullYear(2025));                   // => 2024
+
+
+// console.log(a.getHours());                             // => 8
+// console.log(a.getUTCHours());                          // => 3
+
+
+// console.log(a.getSeconds());                           // => 52
+// console.log(a.getUTCSeconds());                        // => 52
+
+
+// console.log(a.getMinutes());                           //  => 50
+// console.log(a.getUTCMinutes());                        // => 20
+
+
+// console.log(a.getDay());                               // => 5
+// console.log(a.getUTCDay());                            // => 5
+
+
+// console.log(a.getMilliseconds());                      // => 702 ( Page Reload and anytime Change)
+// console.log(a.getUTCMilliseconds());                   // => 702 ( Page Reload and Anytime Change )
+
+
+// console.log(a.getTimezoneOffset());                    // => -330 ( +5:30 Hour )
+
